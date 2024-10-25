6474802e118c1ba54d6e0976e208e0493c4f3dee
mohammed iqbal v <24mcam04@meaec.edu.in>
2024-10-25 14:53:12 +0530

    Create Linkedlist.c

---
+++ Linkedlist.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void deleteNode(struct Node** head, int key) {
+    struct Node* temp = *head;
+    struct Node* prev = NULL;
+    if (temp != NULL && temp->data == key) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL) return;
+    prev->next = temp->next;
+    free(temp);
+}
+
+int searchNode(struct Node* head, int key) {
+    struct Node* current = head;
+    while (current != NULL) {
+        if (current->data == key) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int choice, data, key;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Insert at End\n");
+        printf("2. Delete by Value\n");
+        printf("3. Search by Value\n");
+        printf("4. Display List\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &data);
+                insertEnd(&head, data);
+                printf("%d inserted.\n", data);
+                break;
+            case 2:
+                printf("Enter value to delete: ");
+                scanf("%d", &key);
+                deleteNode(&head, key);
+                printf("%d deleted if it was present.\n", key);
+                break;
+            case 3:
+                printf("Enter value to search: ");
+                scanf("%d", &key);
+                if (searchNode(head, key)) {
+                    printf("%d found in the list.\n", key);
+                } else {
+                    printf("%d not found in the list.\n", key);
+                }
+                break;
+            case 4:
+                printf("Linked list: ");
+                printList(head);
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 5);
+
+    return 0;
+}
